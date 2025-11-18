394829c243f408b0fb91e022254447e917fe0351
c0a24277e4 <c0a24277e4@edu.teu.ac.jp>
2025-11-18 17:37:23 +0900

    11-演習課題

--- pygame_image.py
+++ pygame_image.py
@@ -29,11 +29,13 @@ def main():
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
+            kk_rct.move_ip((0, +2))
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
             kk_rct.move_ip((+1, 0))
+        else:
+            kk_rct.move_ip((-1, 0))
             
         screen.blit(kk_img, kk_rct)
         pg.display.update()
